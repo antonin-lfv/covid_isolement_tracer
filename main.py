6e5479240c152514b1bd1e6619a562e711f124d0
antonin-lfv <antoninlefevre45@icloud.com>
2022-01-16 01:22:28 +0100

    security

--- main.py
+++ main.py
@@ -20,6 +20,8 @@ st.markdown("""
     // border: solid #4976E4 5px;
     // padding: 5px;
 }
+.row_heading.level0 {display:none}
+.blank {display:none}
 </style>
 """, unsafe_allow_html=True)
 st.markdown('<p class="first_titre">Covid Tracer</p>', unsafe_allow_html=True)
@@ -43,8 +45,7 @@ def run_query(query):
 
 
 def run_update_query(query):
-    with conn.cursor() as cur:
-        cur.execute(query)
+    conn.cursor().execute(query)
     conn.commit()
 
 
@@ -57,7 +58,7 @@ def get_data(request, con=conn):
 def liste_classe(prof):
     """Retourn les infos des élèves du prof"""
     return get_data(
-        f"SELECT Nom_eleve, Prenom_eleve, Debut_isolement, Durée_isolement from Eleve, Professeur WHERE Eleve.id_professeur=Professeur.id_professeur AND Professeur.Nom_professeur='{prof}'")
+        f"SELECT Nom_eleve AS 'Nom', Prenom_eleve AS 'Prenom', Debut_isolement AS 'Début isolement', Durée_isolement AS 'Durée isolement' from Eleve, Professeur WHERE Eleve.id_professeur=Professeur.id_professeur AND Professeur.Nom_professeur='{prof}'")
 
 
 def liste_prof():
@@ -65,8 +66,8 @@ def liste_prof():
     return get_data(f"SELECT Nom_professeur, Prenom_professeur, id_professeur from Professeur")
 
 
-def update_isolement(nom_eleve, prenom_eleve, debut_isol, duree):
-    sql = f"UPDATE Eleve SET Debut_isolement = '{debut_isol}', Durée_isolement = '{duree}' WHERE Nom_eleve = '{nom_eleve}' AND Prenom_eleve = '{prenom_eleve}'"
+def update_isolement(nom_eleve, prenom_eleve, debut_isol, duree, prof):
+    sql = f"UPDATE Eleve SET Debut_isolement = '{debut_isol}', Durée_isolement = '{duree}' WHERE Nom_eleve = '{nom_eleve}' AND Prenom_eleve = '{prenom_eleve}' AND id_professeur = (SELECT id_professeur FROM Professeur WHERE Nom_professeur='{prof}')"
     return run_update_query(sql)
 
 
@@ -78,32 +79,31 @@ with c1:
 
 if prof != '-- Nom du professeur --':
     with c3:
-        st.write(liste_classe(prof))
+        st.dataframe(liste_classe(prof))
 
     with c1:
         st.write("##")
         with st.form("Modifier ou ajouter une date d'isolement"):
             st.write("Modifier ou ajouter une date d'isolement")
-            eleve = st.selectbox(options=[" ".join((i, j)) for i, j in zip(list(liste_classe(prof)['Nom_eleve']),
-                                                                           list(liste_classe(prof)['Prenom_eleve']))],
+            eleve = st.selectbox(options=[" ".join((i, j)) for i, j in zip(list(liste_classe(prof)['Nom']),
+                                                                           list(liste_classe(prof)['Prenom']))],
                                  label='Choisir un élève')
             debut_isol = st.date_input(label="Date de début d'isolement", max_value=date.today(), help="Format date = AAAA/MM/JJ")
             duree = st.number_input(label="Durée d'isolement", min_value=1, max_value=30, help="Nombre de jours d'isolement")
             submitted = st.form_submit_button("Submit")
             if submitted:
-                update_isolement(eleve.split(" ")[0], eleve.split(" ")[1], debut_isol, duree)
+                update_isolement(eleve.split(" ")[0], eleve.split(" ")[1], debut_isol, duree, prof)
                 st.success("Modification effectuée avec succès !")
 
         st.write("##")
-        date_fin_isole = st.date_input(label="Elève en fin d'isolement ce jour", min_value=date.today(), help="Format date = AAAA/MM/JJ")
+        date_fin_isole = st.date_input(label="Elève(s) en fin d'isolement ce jour", min_value=date.today(), help="Format date = AAAA/MM/JJ")
         st.write("##")
         res_date_fin_isole = get_data(
-            f"SELECT Nom_eleve, Prenom_eleve from Eleve, Professeur WHERE Eleve.id_professeur=Professeur.id_professeur AND Professeur.Nom_professeur='{prof}' AND DATE_ADD(Debut_isolement, INTERVAL Durée_isolement DAY)='{date_fin_isole}'")
+            f"SELECT Nom_eleve AS 'Nom', Prenom_eleve AS 'Prénom' from Eleve, Professeur WHERE Eleve.id_professeur=Professeur.id_professeur AND Professeur.Nom_professeur='{prof}' AND DATE_ADD(Debut_isolement, INTERVAL Durée_isolement DAY)='{date_fin_isole}'")
         if len(res_date_fin_isole) == 0:
             st.info("Aucun élève ne rentre ce jour")
         else:
-            st.write(res_date_fin_isole)
+            st.dataframe(res_date_fin_isole)
 
+    st.write("##")
     st.write("---")
-
-    # st.write(liste_prof())
